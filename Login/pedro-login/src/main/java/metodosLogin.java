7d9d220745f26c132e59e752f6b96084f21214c7
amandaribeiro1 <amanda.costa@sptech.school>
2024-04-28 19:29:26 -0300

    Resolvendo conflitos

--- Login/pedro-login/src/main/java/metodosLogin.java
+++
@@ -1,9 +0,0 @@
-public class metodosLogin {
-
-    public static boolean validateLogin(String username, String password) {
-        return username.equals("pedroartur") && password.equals("123456");
-    }
-}
-
-
-
